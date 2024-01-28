7bc572e4078c4a9649cc95305721fd8bfa19db95
Guangjian Bao <guangjianbao@gmail.com>
2024-01-28 16:05:42 -0500

    update url

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("https://jian-8h95.onrender.com")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
